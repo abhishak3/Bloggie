dea94944456bc07011089264c600b52687756902
abhishak3 <abhishek.pandey.3237@gmail.com>
2022-06-29 21:45:27 +0530

    added view tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+class BlogTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = "testuser",
+            email = "test@email.com",
+            password = "secret"
+        )
+        self.post = Post.objects.create(
+            title = 'A good title',
+            text = 'Nice body content',
+            author = self.user
+        )
+    
+    def test_string_representation(self):
+        post = Post(title='A simple title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.text}', 'Nice body content')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+
+    def test_home_page_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/blog/1')
+        no_response = self.client.get('/blog/10000000')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
