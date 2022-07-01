1bd6c8c84965ee1431be51de911be937c0d86447
abhishak3 <abhishek.pandey.3237@gmail.com>
2022-07-01 18:01:35 +0530

    implemented CRUD for blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import HomePageView, BlogDetailView
+from .views import (
+    HomePageView, 
+    BlogDetailView, 
+    BlogCreateView, 
+    BlogEditView,
+    BlogDeleteView
+)
 
 urlpatterns = [
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
+    path('blog/<int:pk>/edit/', BlogEditView.as_view(), name='post_edit'),
+    path('blog/new/', BlogCreateView.as_view(), name='post_new'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', HomePageView.as_view(), name='home'),
-    path('blog/<int:pk>', BlogDetailView.as_view(), name='post_detail')
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -10,4 +16,19 @@ class HomePageView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__' # all fields of model Post
+
+class BlogEditView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'text'] # we don't want author to change
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home') # redirects only when deletion is completed
 
